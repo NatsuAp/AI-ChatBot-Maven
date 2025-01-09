665b08b51bccc2081bb94aea9049d82140f736a5
natsu <andrewtt824@gmail.com>
2025-01-09 11:47:37 -0500

    OpenAi API

--- src/main/java/org/example/BackEnd/APIClient.java
+++ src/main/java/org/example/BackEnd/APIClient.java
@@ -1,16 +1,16 @@
 package org.example.BackEnd;
-import com.openai.client.OpenAIClient;
-import com.openai.client.okhttp.OpenAIOkHttpClient;
 import com.openai.models.ChatCompletion;
 import com.openai.models.ChatCompletionCreateParams;
 import com.openai.models.ChatCompletionMessageParam;
 import com.openai.models.ChatCompletionUserMessageParam;
 import com.openai.models.ChatModel;
+import com.openai.client.OpenAIClient;
+import com.openai.client.okhttp.OpenAIOkHttpClient;
 import java.util.List;
 
 public class APIClient {
 
-    String apiKey = "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";
+    String apiKey = "";
 
     OpenAIClient client = OpenAIOkHttpClient.builder()
             .apiKey(apiKey)
