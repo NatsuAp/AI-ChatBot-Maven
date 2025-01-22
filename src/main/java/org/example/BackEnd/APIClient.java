99860968831455a94b3f810c84aa0ff7b02ad3d1
natsu <andrewtt824@gmail.com>
2025-01-22 18:35:40 -0500

    ApiClient

--- src/main/java/org/example/BackEnd/APIClient.java
+++ src/main/java/org/example/BackEnd/APIClient.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class APIClient {
 
-     String apiKey =;
+     String apiKey =
 
     OpenAIClient client = OpenAIOkHttpClient.builder()
             .apiKey(apiKey)
