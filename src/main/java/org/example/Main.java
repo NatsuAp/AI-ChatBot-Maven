f4a059b2b0524abd1ac3affbea8752ea5e92f98f
natsu <andrewtt824@gmail.com>
2025-01-22 18:32:36 -0500

    python scripts

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ public class Main {
         FrameBuilder in = new FrameBuilder(); // Se accede a la funcion que construye el frame
         in.frameSetup(); //se llama a la funcion
         APIClient temp = new APIClient();
+        temp.AIResponse();
 
     }
 
