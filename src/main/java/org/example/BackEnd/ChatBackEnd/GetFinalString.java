8d65eae94c925473fe7392fa8c1ff6ab27376d1e
Technology Rooms <technologyrooms@colsanjose.edu.co>
2025-03-10 15:05:04 -0500

    promptlista

--- src/main/java/org/example/BackEnd/ChatBackEnd/GetFinalString.java
+++ src/main/java/org/example/BackEnd/ChatBackEnd/GetFinalString.java
@@ -1,13 +1,26 @@
 package org.example.BackEnd.ChatBackEnd;
 
+import org.example.BackEnd.Embeddings.EmbeddingsRequests;
+import org.example.BackEnd.Setup.CompareEmb;
+
 public class GetFinalString {
-    public String main(String[] emb, String userInput) {
-        String concatenado = "";
-        for(String x: emb){
-            concatenado = x + "--" + concatenado;
+    public String userInput;
+    CompareEmb comp = new CompareEmb();
+    EmbeddingsRequests emb = new EmbeddingsRequests();
+    public String getPrompt(String userInput) {
+        this.userInput = userInput;
+        String prompt = userInput + " //  Aca tienes algunos ejemplos para que te guies con tu respuesta: EJEMPLO 1 : ";
+        String[][] preguntas = new String[5][5];
+        int i = 1;
+        for(String[] x: preguntas){
+            for(String q: x){
+                prompt = prompt + " " + q;
+            }
+            prompt = prompt + "EJEMPLO " + i + ":"; 
         }
-        String res = userInput + "Aca tienes algunos ejemplos de preguntas similares para que tengas una guia" + concatenado;
-        return "";
+
+        
+        return prompt;
         
     }
 }
