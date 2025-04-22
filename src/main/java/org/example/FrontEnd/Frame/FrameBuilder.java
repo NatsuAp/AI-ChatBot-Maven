3c18fb53fa228edbed6bad6eb799ee5f78cb8b59
Technology Rooms <technologyrooms@colsanjose.edu.co>
2025-04-22 11:15:03 -0500

    Messagepanels

--- src/main/java/org/example/FrontEnd/Frame/FrameBuilder.java
+++ src/main/java/org/example/FrontEnd/Frame/FrameBuilder.java
@@ -1,38 +1,49 @@
 package org.example.FrontEnd.Frame;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
+import javax.swing.border.Border;
 
 import org.example.FrontEnd.Labels.HeaderLabel;
-import org.example.FrontEnd.Panels.*;
-;
+import org.example.FrontEnd.Panels.*;;
 
 public class FrameBuilder {
     // public JTextField textArea;
     public void frameSetup() {
         HeaderPanel headerP = new HeaderPanel(); //
+
         SearchboxPanel searchboxP = new SearchboxPanel();// importacion archivos
+
         HeaderLabel headerL = new HeaderLabel();//
 
         BodyPanel bodyP = new BodyPanel();
 
         JFrame frame = Frame.createFrame();//
+
         JPanel headerPanel = headerP.createHeader();//
+
         JLabel headerLabel = headerL.headerLabel();//
+
         JPanel searchPanel = searchboxP.createSearchbox();// importacion metodos
 
         JPanel bodyPanel = bodyP.Body();
-
+        JPanel msg = MessagePanel.messagePanel();
+        bodyPanel.add(msg,BorderLayout.CENTER);
         headerPanel.add(headerLabel);
-
+        
         frame.add(searchPanel, BorderLayout.SOUTH);
-        frame.add(headerPanel, BorderLayout.NORTH); // se añaden los paneles al frame
-        frame.add(bodyPanel, BorderLayout.CENTER);
 
+       
+        frame.add(headerPanel, BorderLayout.NORTH); // se añaden los paneles al frame
+
+        frame.add(bodyPanel, BorderLayout.CENTER);
+        frame.pack();
         frame.setVisible(true);
 
     }
